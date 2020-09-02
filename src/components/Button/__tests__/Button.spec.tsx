2ec9b28c624701a64181dd2f37cbc3d2107c020a
Ryan Pelham <pelhamrj@gmail.com>
2020-09-02 16:35:24 -0400

    Fixed testing issue

--- src/components/Button/__tests__/Button.spec.tsx
+++ src/components/Button/__tests__/Button.spec.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+
+import '../../../mocks/matchMedia.mock';
+
 import Button from '../Button';
 
 test('Renders', async () => {
