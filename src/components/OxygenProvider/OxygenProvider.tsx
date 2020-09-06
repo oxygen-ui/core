12aeea97d48efa0e3bae2e3a55e84606526f8e22
Ryan Pelham <pelhamrj@gmail.com>
2020-09-06 09:26:19 -0400

    Added normalize

--- src/components/OxygenProvider/OxygenProvider.tsx
+++ src/components/OxygenProvider/OxygenProvider.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react';
+import 'normalize.css';
 
 import Theme, { theming, defaultTheme, ThemeType } from '../../theme';
 import { ThemeContextProvider } from '../../theme/ThemeContext';
