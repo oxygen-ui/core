82d18fdf5101140ca7b2a9e1131543f959ee11f9
Ryan Pelham <pelhamrj@gmail.com>
2020-09-07 17:06:29 -0400

    Added loading indicators

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -102,3 +102,104 @@ export const actions = (): JSX.Element => (
         Hello
     </Button>
 );
+
+export const growLoadingColors = (): JSX.Element => (
+    <div>
+        <Button style={{ marginRight: 5 }} isLoading={true} color="primary">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="secondary">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="success">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="info">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="warning">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="error">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="light">
+            Please wait...
+        </Button>
+        <Button style={{ marginLeft: 5 }} isLoading={true} color="dark">
+            Please wait...
+        </Button>
+    </div>
+);
+
+export const growLoadingSizes = (): JSX.Element => (
+    <div>
+        <Button style={{ marginRight: 10 }} isLoading={true} color="primary" size="small">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="primary" size="medium">
+            Please wait...
+        </Button>
+        <Button style={{ marginLeft: 10 }} isLoading={true} color="primary" size="large">
+            Please wait...
+        </Button>
+    </div>
+);
+
+export const spinLoadingColors = (): JSX.Element => (
+    <div>
+        <Button style={{ marginRight: 5 }} isLoading={true} color="primary" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="secondary" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="success" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="info" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="warning" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="error" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ margin: 10 }} isLoading={true} color="light" loadingType="spin">
+            Please wait...
+        </Button>
+        <Button style={{ marginLeft: 5 }} isLoading={true} color="dark" loadingType="spin">
+            Please wait...
+        </Button>
+    </div>
+);
+
+export const spinLoadingSizes = (): JSX.Element => (
+    <div>
+        <Button
+            style={{ marginRight: 10 }}
+            isLoading={true}
+            color="primary"
+            size="small"
+            loadingType="spin">
+            Please wait...
+        </Button>
+        <Button
+            style={{ margin: 10 }}
+            isLoading={true}
+            color="primary"
+            size="medium"
+            loadingType="spin">
+            Please wait...
+        </Button>
+        <Button
+            style={{ marginLeft: 10 }}
+            isLoading={true}
+            color="primary"
+            size="large"
+            loadingType="spin">
+            Please wait...
+        </Button>
+    </div>
+);
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,11 @@
-import React, { forwardRef, ComponentPropsWithoutRef, useContext } from 'react';
+import React, { forwardRef, ComponentPropsWithoutRef, useContext, CSSProperties } from 'react';
 import classNames from 'classnames';
 
 import { ThemeContext, Color, Size } from '../../theme';
 import ParseThemeType from '../../utils/ParseThemeType';
 import { useStyles } from './Button.styles';
+import Spinner from '../Spinner';
+import ColorParser from 'color';
 
 export interface ButtonProps {
     block?: boolean;
@@ -11,14 +13,18 @@ export interface ButtonProps {
     color?: Color;
     disabled?: boolean;
     isLoading?: boolean;
+    loadingType?: 'grow' | 'spin';
     size?: Size;
 }
 
 type CombinedProps = ButtonProps & ComponentPropsWithoutRef<'button'>;
 
 const Button = forwardRef<HTMLButtonElement, CombinedProps>(
-    ({ block, children, className, color, disabled, isLoading, size, ...props }, ref) => {
-        const { themeType } = useContext(ThemeContext);
+    (
+        { block, children, className, color, disabled, isLoading, loadingType, size, ...props },
+        ref
+    ) => {
+        const { theme, themeType } = useContext(ThemeContext);
         const classes = useStyles({
             block,
             color,
@@ -28,12 +34,60 @@ const Button = forwardRef<HTMLButtonElement, CombinedProps>(
             themeType: ParseThemeType(themeType),
         });
 
+        const loadingStyles: CSSProperties = {
+            marginRight: 10,
+            width: '0.875em',
+            height: '0.875em',
+        };
+        const parsedLoadingType = loadingType || 'grow';
+
+        if (parsedLoadingType === 'spin') {
+            loadingStyles.width = '0.6em';
+            loadingStyles.height = '0.6em';
+            loadingStyles.borderWidth = 2;
+        }
+
+        if (size === 'small' && parsedLoadingType === 'grow') {
+            loadingStyles.width = '0.74em';
+            loadingStyles.height = '0.74em';
+        }
+
+        if (size === 'small' && parsedLoadingType === 'spin') {
+            loadingStyles.width = '0.5em';
+            loadingStyles.height = '0.5em';
+            loadingStyles.borderWidth = 1.5;
+        }
+
+        if (size === 'large' && parsedLoadingType === 'grow') {
+            loadingStyles.width = '0.95em';
+            loadingStyles.height = '0.95em';
+        }
+
+        if (size === 'large' && parsedLoadingType === 'spin') {
+            loadingStyles.width = '0.65em';
+            loadingStyles.height = '0.65em';
+            loadingStyles.borderWidth = 3;
+        }
+
         return (
             <button
                 ref={ref}
                 className={classNames(classes.button, className)}
                 disabled={disabled}
                 {...props}>
+                {isLoading && (
+                    <Spinner
+                        style={loadingStyles}
+                        type={parsedLoadingType}
+                        color={
+                            ColorParser(
+                                theme.colors[ParseThemeType(themeType)].background[color || 'light']
+                            ).isDark()
+                                ? 'light'
+                                : 'dark'
+                        }
+                    />
+                )}
                 {children}
             </button>
         );
