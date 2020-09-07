80c3162b3819072daeddb73afab01923cf583b82
Ryan Pelham <pelhamrj@gmail.com>
2020-09-07 17:27:57 -0400

    Fixed build error

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
 
 import pkg from './package.json';
 
@@ -25,6 +26,7 @@ export default {
     plugins: [
         external(),
         resolve(),
+        postcss(),
         typescript({
             rollupCommonJSResolveHack: true,
             exclude: '**/__tests__/**',
